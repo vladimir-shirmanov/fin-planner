17e7b62cc086b35fb79977d2b5351ceaf9396fea
Volodymyr Shyrmanov <volodymyr.shyrmanov@nxis.com>
2025-01-17 11:55:17 +0100

    added ui part

--- budget/app/main.py
+++ budget/app/main.py
@@ -1,6 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from typing import Optional
+from urllib.parse import urljoin
 
 app = FastAPI()
+
+class Category:
+    id: int
+    name: str
+
+class Pagination:
+    page: int
+    per_page: int
+    next_page: str
+    prev_page: Optional[str]
+    items: list
+    def __init__(self, page: int, per_page: int, items: list, url = None):
+        self.page = page
+        self.per_page = per_page
+        self.items = items
+        if page == 0:
+            self.prev_page = None
+        else:
+            self.prev_page = '{}?page={}&per_page={}'.format(url, page - 1, per_page)
+        self.next_page = '{}?page={}&per_page={}'.format(url, page + 1, per_page)
+
+
+fake_items = [{'id': i, 'name': 'task {}'.format(i)} for i in range(20)]
+
+
+@app.get('/items/')
+async def get_items(request: Request, page: int = 1, per_page: int = 3):
+    result = Pagination(page, per_page, fake_items[(page-1)*per_page: page*per_page], urljoin(str(request.base_url), request.url.path))
+    return result
 
 @app.get("/")
 async def root():
--- budget/hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from budget!")
-
-
-if __name__ == "__main__":
-    main()
